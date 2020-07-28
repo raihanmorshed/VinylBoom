b7689f297933affd0d908ba3ee8ac528990886a2
Raihan Morshed <raihanmorshed@Raihans-MacBook-Air.local>
2020-07-28 15:04:02 -0400

    like button added

--- src/Components/Records.jsx
+++ src/Components/Records.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Like from "./Common/Like";
 import { getRecords } from "../services/fakeRecordService";
 
 class Records extends Component {
@@ -12,36 +13,47 @@ class Records extends Component {
   };
 
   render() {
+    const { length: count } = this.state.records;
+
+    if (count === 0) return <p>There are no records in the database. </p>;
+
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title </th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>RIAA Certification</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.records.map((record) => (
-            <tr key={record._id}>
-              <td>{record.title}</td>
-              <td>{record.genre.name}</td>
-              <td>{record.numberInStock}</td>
-              <td>{record.dailyRentalRate}</td>
-              <td>
-                <button
-                  onClick={() => this.handleDelete(record)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
+      <React.Fragment>
+        <p>Showing {count} records in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title </th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>RIAA Certification</th>
+              <th />
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.records.map((record) => (
+              <tr key={record._id}>
+                <td>{record.title}</td>
+                <td>{record.genre.name}</td>
+                <td>{record.numberInStock}</td>
+                <td>{record.dailyRentalRate}</td>
+                <td>
+                  <Like liked={record.liked} />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(record)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
